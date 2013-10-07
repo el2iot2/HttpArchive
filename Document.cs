37d7159d4ffadba0f300f1ed60e320e470c0ea3b
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2013-10-06 22:27:06 -0700

    Root Document and nuget attribute updates

---
+++ Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Automatonic.HttpArchive
+{
+    /// <summary>
+    /// This object represents the document wrapper needed to load the log file.
+    /// </summary>
+    [JsonObject]
+    public class Document
+    {
+        /// <summary>
+        /// Version number of the format. If empty, string "1.1" is assumed by default.
+        /// </summary>
+        [JsonProperty(PropertyName = "log")]
+        public Log Log { get; set; }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HttpArchive")]
-[assembly: AssemblyDescription(".Net types for HTTP Archive (HAR) specification (version 1.2) ")]
+[assembly: AssemblyTitle("Automatonic.HttpArchive")]
+[assembly: AssemblyDescription(".Net types for serialization of the HTTP Archive (HAR) specification (version 1.2)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://automatonic.net")]
-[assembly: AssemblyProduct("HttpArchive")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCompany("Elliott B. Edwards (automatonic)")]
+[assembly: AssemblyProduct("Automatonic.HttpArchive")]
+[assembly: AssemblyCopyright("Copyright © 2013, Elliott B. Edwards")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.2")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
