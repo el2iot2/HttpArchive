04a00ee62b30cfb038bb14b70ecdaa37533c57a0
Elliott B. Edwards <28941195+el2iot2@users.noreply.github.com>
2019-11-23 09:55:06 -0800

    sample har from chromehar project

--- src/HttpArchive.Tests/DocumentShould.cs
+++ src/HttpArchive.Tests/DocumentShould.cs
@@ -1,6 +1,8 @@
 using System.Text.Json;
 using Xunit;
 using FluentAssertions;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace HttpArchive
 {
@@ -23,6 +25,21 @@ namespace HttpArchive
             deserialized.Log.Pages.Should().BeNull();
             deserialized.Log.Entries.Should().NotBeNull();
             deserialized.Log.Entries.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task LoadSampleHarAsync()
+        {
+            var deserialized = await JsonSerializer.DeserializeAsync<Document>(utf8Json: File.OpenRead("sample.har"));
+            deserialized.Log.Should().NotBeNull();
+            // deserialized.Log.Comment.Should().BeNull();
+            // deserialized.Log.Creator.Should().NotBeNull();
+            // deserialized.Log.Creator.Name.Should().Be("");
+            // deserialized.Log.Creator.Version.Should().Be("");
+            // deserialized.Log.Browser.Should().BeNull();
+            // deserialized.Log.Pages.Should().BeNull();
+            // deserialized.Log.Entries.Should().NotBeNull();
+            // deserialized.Log.Entries.Should().BeEmpty();
         }
     }
 }
